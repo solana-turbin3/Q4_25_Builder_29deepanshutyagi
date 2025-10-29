529575894b8b596517ee39a0751821d40be1e0d3
29deepanshutyagi <29deepanshutyagi@gmail.com>
2025-10-29 21:59:19 +0530

    Add amm folder

---
+++ amm/programs/amm/src/errors.rs
@@ -0,0 +1,44 @@
+use anchor_lang::error_code;
+
+
+#[error_code]
+pub enum AmmError {
+    #[msg("DefaultError")]
+    DefaultError,
+    #[msg("Offer expired.")]
+    OfferExpired,
+    #[msg("This pool is locked.")]
+    PoolLocked,
+    #[msg("Slippage exceeded.")]
+    SlippageExceeded,
+    #[msg("Overflow detected.")]
+    Overflow,
+    #[msg("Underflow detected.")]
+    Underflow,
+    #[msg("Invalid token.")]
+    InvalidToken,
+    #[msg("Actual liquidity is less than minimum.")]
+    LiquidityLessThanMinimum,
+    #[msg("No liquidity in pool.")]
+    NoLiquidityInPool,
+    #[msg("Bump error.")]
+    BumpError,
+    #[msg("Curve error.")]
+    CurveError,
+    #[msg("Fee is greater than 100%. This is not a very good deal.")]
+    InvalidFee,
+    #[msg("Invalid update authority.")]
+    InvalidAuthority,
+    #[msg("No update authority set.")]
+    NoAuthoritySet,
+    #[msg("Invalid amount.")]
+    InvalidAmount,
+    #[msg("Invalid precision.")]
+    InvalidPrecision,
+    #[msg("Insufficient balance.")]
+    InsufficientBalance,
+    #[msg("Zero balance.")]
+    ZeroBalance,
+}
+
+
